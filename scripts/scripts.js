9938ce1cbe87d0bc4c5c1d4bfe10f73142dbf362
Sean Wallace <wallace.sean111@gmail.com>
2022-09-29 17:29:06 +0200

    Add function to receive and check input

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,8 +1,18 @@
+console.log('hello world!');
 // Game module //
 const game = (() => {
-  let gameArray = [];
-  
-  receiveInput
-  checkResult
-  sendResult
+  let gameArray = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+
+  const receiveInput = (square) => {
+    if (gameArray[square] !== 1) {
+      gameArray[square] = 1;
+      return (true);
+    } else {
+      return (false);
+    }
+  };
+
+  return {
+    receiveInput,
+  };
 })();
