f12af80d3a30b0aae419768d351338353b0b1b53
Sean Wallace <wallace.sean111@gmail.com>
2022-09-30 15:32:02 +0200

    Add winner announcement modal

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,8 +1,10 @@
+/* eslint-disable no-alert */
+/* eslint-disable no-restricted-globals */
 /* eslint-disable no-console */
 /* eslint-disable no-unused-vars */
 console.log('hello world!');
 
-// Game module //
+// Game logic module //
 
 const game = (() => {
   const gameArray = [0, 0, 0, 0, 0, 0, 0, 0, 0];
@@ -29,6 +31,10 @@ const game = (() => {
 
   // Checks to see if a player has won. If so, returns the number of that player
   const checkWinner = () => {
+    let winner = -1;
+    if (!gameArray.includes(0)) {
+      winner = 0;
+    }
     const lineValues = {
       horiz0: gameArray[0] + gameArray[1] + gameArray[2],
       horiz1: gameArray[3] + gameArray[4] + gameArray[5],
@@ -42,33 +48,36 @@ const game = (() => {
 
     // Map the values of the above object to an array
     const lineArray = Object.keys(lineValues).map((keys) => lineValues[keys]);
-    let winner = 0;
 
     if (lineArray.includes(3)) { // If a line on the board contains 3 like symbols, declare winner
       winner = 1;
-      alert('Winner is Player One!');
+      // eslint-disable-next-line no-use-before-define
+      display.winnerDeclared(playerArray[0].name);
     } else if (lineArray.includes(12)) {
       winner = 4;
-      alert('Winner is Player Two!');
+      // eslint-disable-next-line no-use-before-define
+      display.winnerDeclared(playerArray[1].name);
+    } else if (winner === 0) {
+      alert('Game is a draw!');
     }
     return (winner);
   };
 
   const controlGame = (() => {
-    let currentPlayer = false; // false is "Player one"/playerArray[0]
-    const idLastPlayer = () => (currentPlayer); // Return the current player 
+    let currentPlayer = false; // false is O
+    const idLastPlayer = () => (currentPlayer); // Return the current player
     // eslint-disable-next-line max-len
-    const processInput = (square) => { // If move valid, marks relevant array position with appropriate number & returns true
+    const processInput = (square) => { // If move valid, adds player number to relevant array position & returns true
       if (gameArray[square] === 0 && currentPlayer === false) {
         gameArray[square] = 1;
         currentPlayer = !currentPlayer;
-        console.log("gameArray[square] = ", gameArray[square]);
+        console.log('gameArray[square] = ', gameArray[square]);
         checkWinner();
         return (true);
       } if (gameArray[square] === 0 && currentPlayer === true) {
         gameArray[square] = 4;
         currentPlayer = !currentPlayer;
-        console.log("gameArray[square] = ", gameArray[square]);
+        console.log('gameArray[square] = ', gameArray[square]);
         checkWinner();
         return (true);
       }
@@ -76,7 +85,7 @@ const game = (() => {
       return (false);
     };
 
-    const inputOutput = (id) => { 
+    const inputOutput = (id) => {
       const result = processInput(id); // Passes the ID of a clicked div to be processed
       return (result); // Returns turn result to display function to update display as appropriate
     };
@@ -93,12 +102,14 @@ const game = (() => {
   };
 })();
 
-// Display and input
+// Display and input module //
+
 const display = (() => {
+  const modal = document.querySelector('.modal');
   const updateDisplay = (squareId) => {
-    const player = game.controlGame.idLastPlayer(); // Gets the ID of the player who LAST played
+    const player = game.controlGame.idLastPlayer(); // Gets the ID of the player who last played
     const selectSquare = document.querySelector(`.square${squareId}`);
-    if (player === true) {
+    if (player === true) { // If player === true, the last player to play was player one
       const nought = document.createElement('img');
       nought.setAttribute('src', 'resources/nought.svg');
       selectSquare.append(nought);
@@ -117,18 +128,31 @@ const display = (() => {
     // eslint-disable-next-line prefer-destructuring
     const squareId = e.target.id;
     const result = game.controlGame.inputOutput(squareId);
-    if (result === true) {
+    if (result === true) { // If last move was valid, update the display
       const displayUpdate = updateDisplay(squareId);
       console.log(displayUpdate);
     }
     console.log(result);
   };
+  
+  const toggleModal = () => {
+    modal.classList.toggle('show-modal');
+  };
+
+  const windowOnClick = (e) => {
+    if (e.target === modal) {
+      toggleModal();
+    }
+  };
 
   const createDisplay = () => { // Creates game board divs and adds event listeners
     function makeSquare(i) {
       const gameContainer = document.querySelector('.game-container');
+      const modalCloseButton = document.querySelector('.modal-close-button');
       const square = document.createElement('div');
       square.addEventListener('click', getInput);
+      window.addEventListener('click', windowOnClick);
+      modalCloseButton.addEventListener('click', toggleModal);
       square.classList.add('square', `square${i}`);
       square.setAttribute('id', i);
       gameContainer.append(square);
@@ -138,10 +162,15 @@ const display = (() => {
     }
   };
 
-  const removeListeners = () => {
+  const winnerDeclared = (winner) => {
+    console.log("Winner declared!");
     const selectSquares = document.querySelectorAll('.square');
     selectSquares.forEach((square) => square.removeEventListener('click', getInput));
-    console.log(selectSquares);
+    const selectModalContent = document.querySelector('.modal-content');
+    const newH2 = document.createElement('h2');
+    newH2.textContent = `${winner} is the winner!`;
+    selectModalContent.append(newH2);
+    modal.classList.toggle('show-modal');
   };
 
   const resetGame = () => {
@@ -149,9 +178,9 @@ const display = (() => {
   };
 
   createDisplay();
-
   return {
-    removeListeners,
+    winnerDeclared,
     resetGame,
+    toggleModal,
   };
 })();
